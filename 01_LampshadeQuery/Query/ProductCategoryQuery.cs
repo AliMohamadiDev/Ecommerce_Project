390918afb69505e8388c0095662fd68a9b13c075
Ali <pachinco.a.m@gmail.com>
2022-08-03 17:58:10 +0430

    Our Product Section Implemented

--- 01_LampshadeQuery/Query/ProductCategoryQuery.cs
+++ 01_LampshadeQuery/Query/ProductCategoryQuery.cs
@@ -1,4 +1,9 @@
-﻿using _01_LampshadeQuery.Contracts.ProductCategory;
+﻿using _0_Framework.Application;
+using _01_LampshadeQuery.Contracts.Product;
+using _01_LampshadeQuery.Contracts.ProductCategory;
+using InventoryManagement.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Infrastructure.EFCore;
 
 namespace _01_LampshadeQuery.Query;
@@ -6,10 +11,12 @@ namespace _01_LampshadeQuery.Query;
 public class ProductCategoryQuery : IProductCategoryQuery
 {
     private readonly ShopContext _shopContext;
+    private readonly InventoryContext _inventoryContext;
 
-    public ProductCategoryQuery(ShopContext shopContext)
+    public ProductCategoryQuery(ShopContext shopContext, InventoryContext inventoryContext)
     {
         _shopContext = shopContext;
+        _inventoryContext = inventoryContext;
     }
 
     public List<ProductCategoryQueryModel> GetProductCategories()
@@ -22,6 +29,47 @@ public class ProductCategoryQuery : IProductCategoryQuery
             PictureAlt = x.PictureAlt,
             PictureTitle = x.PictureTitle,
             Slug = x.Slug
+        }).ToList();
+    }
+
+    public List<ProductCategoryQueryModel> GetProductCategoriesWithProducts()
+    {
+        var inventory = _inventoryContext.Inventory.Select(x =>
+            new {x.ProductId, x.UnitPrice}).ToList();
+
+        var categories = _shopContext.ProductCategories
+            .Include(x => x.Products)
+            .ThenInclude(x => x.Category)
+            .Select(x => new ProductCategoryQueryModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Products = MapProducts(x.Products)
+            }).ToList();
+
+        foreach (var category in categories)
+        {
+            foreach (var product in category.Products)
+            {
+                product.Price = inventory.FirstOrDefault(x => x.ProductId == product.Id)?
+                    .UnitPrice.ToMoney()!;
+            }
+        }
+
+        return categories;
+    }
+
+    private static List<ProductQueryModel> MapProducts(List<Product> products)
+    {
+        return products.Select(product => new ProductQueryModel
+        {
+            Id = product.Id,
+            Category = product.Category.Name,
+            Name = product.Name,
+            Picture = product.Picture,
+            PictureAlt = product.PictureAlt,
+            PictureTitle = product.PictureTitle,
+            Slug = product.Slug
         }).ToList();
     }
 }
