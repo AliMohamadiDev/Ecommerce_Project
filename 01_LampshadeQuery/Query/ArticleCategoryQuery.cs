f39709ab674216a4f5a6fd606f859779ba253923
Ali <pachinco.a.m@gmail.com>
2022-08-10 16:52:00 +0430

    ArticleCategory page implemented

--- 01_LampshadeQuery/Query/ArticleCategoryQuery.cs
+++ 01_LampshadeQuery/Query/ArticleCategoryQuery.cs
@@ -18,7 +18,8 @@ public class ArticleCategoryQuery : IArticleCategoryQuery
 
     public ArticleCategoryQueryModel GetArticleCategory(string slug)
     {
-        return _blogContext.ArticleCategories
+        var articleCategory = _blogContext.ArticleCategories
+            .Include(x=>x.Articles)
             .Select(x => new ArticleCategoryQueryModel
             {
                 Slug = x.Slug,
@@ -30,8 +31,13 @@ public class ArticleCategoryQuery : IArticleCategoryQuery
                 Keywords = x.Keywords,
                 MetaDescription = x.MetaDescription,
                 CanonicalAddress = x.CanonicalAddress,
+                ArticlesCount = x.Articles.Count,
                 Articles = MapArticles(x.Articles)
             }).FirstOrDefault(x => x.Slug == slug)!;
+
+        articleCategory.KeywordList = articleCategory.Keywords.Split(",").ToList();
+
+        return articleCategory;
     }
 
     private static List<ArticleQueryModel> MapArticles(List<Article> articles)
