10f33b92634f0b3cf5dfc5856c2600629b0eaa34
Ali <pachinco.a.m@gmail.com>
2022-08-10 12:26:44 +0430

    LatestArticles section implemented

---
+++ 01_LampshadeQuery/Query/ArticleQuery.cs
@@ -0,0 +1,40 @@
+﻿using _0_Framework.Application;
+using _01_LampshadeQuery.Contracts.Article;
+using BlogManagement.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _01_LampshadeQuery.Query;
+
+public class ArticleQuery : IArticleQuery
+{
+    private readonly BlogContext _blogContext;
+
+    public ArticleQuery(BlogContext blogContext)
+    {
+        _blogContext = blogContext;
+    }
+
+    public List<ArticleQueryModel> LatestArticles()
+    {
+        return _blogContext.Articles
+            .Include(x => x.Category)
+            .Where(x => x.PublishDate <= DateTime.Now)
+            .Select(x => new ArticleQueryModel
+            {
+                CategoryId = x.CategoryId,
+                CategorySlug = x.Category.Slug,
+                CategoryName = x.Category.Name,
+                Slug = x.Slug,
+                CanonicalAddress = x.CanonicalAddress,
+                Description = x.Description,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                PublishDate = x.PublishDate.ToFarsi(),
+                ShortDescription = x.ShortDescription,
+                Title = x.Title
+            }).ToList();
+    }
+}
