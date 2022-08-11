4a42b01a132bf16bc936aa41d444f78120256462
Ali <pachinco.a.m@gmail.com>
2022-08-11 17:57:11 +0430

    Account model created

--- 0_Framework/Application/ApplicationMessages.cs
+++ 0_Framework/Application/ApplicationMessages.cs
@@ -2,6 +2,7 @@
 
 public class ApplicationMessages
 {
-    public const string DuplicatedRecord = "امکان ثبت رکورد تکراری وجود ندارد. لطفا مجددا تلاش بفرمایید.";
-    public const string RecordNotFound = "رکورد با اطلاعات درخواست شده یافت نشد.لطفا مجددا تلاش بفرمایید.";
+    public const string DuplicatedRecord = "امکان ثبت رکورد تکراری وجود ندارد. لطفا مجددا تلاش بفرمایید";
+    public const string RecordNotFound = "رکورد با اطلاعات درخواست شده یافت نشد.لطفا مجددا تلاش بفرمایید";
+    public const string PasswordsNotMatch = "پسورد و تکرار آن با هم مطابقت ندارند";
 }
---
+++ AccountManagement.Application/AccountApplication.cs
@@ -0,0 +1,81 @@
+﻿using _0_Framework.Application;
+using AccountManagement.Application.Contracts.Account;
+using AccountManagement.Domain.AccountAgg;
+
+namespace AccountManagement.Application
+{
+    public class AccountApplication : IAccountApplication
+    {
+        private readonly IFileUploader _fileUploader;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountApplication(IAccountRepository accountRepository, IFileUploader fileUploader, IPasswordHasher passwordHasher)
+        {
+            _fileUploader = fileUploader;
+            _passwordHasher = passwordHasher;
+            _accountRepository = accountRepository;
+        }
+
+        public OperationResult Create(CreateAccount command)
+        {
+            var operation = new OperationResult();
+
+            if (_accountRepository.Exists(x => x.Username == command.Username || x.Mobile == command.Mobile))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+
+            var password = _passwordHasher.Hash(command.Password);
+            var path = $"profilePhotos";
+            var picturePath = _fileUploader.Upload(command.ProfilePhoto, path);
+            var account = new Account(command.Fullname, command.Username, password, command.Mobile, command.RoleId,
+                picturePath);
+            _accountRepository.Create(account);
+            _accountRepository.SaveChanges();
+            return operation.Succeeded();
+        }
+
+        public OperationResult Edit(EditAccount command)
+        {
+            var operation = new OperationResult();
+            var account = _accountRepository.Get(command.Id);
+
+            if (account is null)
+                operation.Failed(ApplicationMessages.RecordNotFound);
+
+            if (_accountRepository.Exists(x => (x.Username == command.Username || x.Mobile == command.Mobile) && x.Id != command.Id))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+
+            var path = $"profilePhotos";
+            var picturePath = _fileUploader.Upload(command.ProfilePhoto, path);
+            account?.Edit(command.Fullname, command.Username, command.Mobile, command.RoleId, picturePath);
+            _accountRepository.SaveChanges();
+            return operation.Succeeded();
+        }
+
+        public OperationResult ChangePassword(ChangePassword command)
+        {
+            var operation = new OperationResult();
+            var account = _accountRepository.Get(command.Id);
+            if (account is null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            if (command.Password != command.RePassword)
+                return operation.Failed(ApplicationMessages.PasswordsNotMatch);
+
+            var password = _passwordHasher.Hash(command.Password);
+            account.ChangePassword(password);
+            _accountRepository.SaveChanges();
+            return operation.Succeeded();
+        }
+
+        public EditAccount GetDetails(long id)
+        {
+            return _accountRepository.GetDetails(id);
+        }
+
+        public List<AccountViewModel> Search(AccountSearchModel searchModel)
+        {
+            return _accountRepository.Search(searchModel);
+        }
+    }
+}
